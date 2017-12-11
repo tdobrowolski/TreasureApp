68edcf9fa94c12a17f79fa2932c4d98c0134e749
tdobrowolski <tobdob15@gmail.com>
2017-12-11 14:32:06 +0100

    Wersja 0.2.5
    + dodanie przycisku zatwierdzającego NewEntry
    + poprawki w wyglądzie NewEntryActivity

--- app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
+++ app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
@@ -3,6 +3,8 @@ package com.example.tobiaszdobrowo.codenametreasure;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -48,6 +50,30 @@ public class NewEntryActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_new_entry, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_check:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
 
