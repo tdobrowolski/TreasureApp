09b0461d2bb8cd441bd106d5497cb657eefc5cf8
tdobrowolski <tobdob15@gmail.com>
2017-12-12 09:40:10 +0100

    Wersja 0.3
    + dodanie nowej aktywności do wyboru daty i zarządzania powiadomienia
    + lekka zmiana layout'u

--- app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/MainActivity.java
+++ app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -80,4 +81,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
+++ app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
@@ -1,50 +1,33 @@
 package com.example.tobiaszdobrowo.codenametreasure;
 
-import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-
-import java.util.Calendar;
+import android.widget.TextView;
 
 public class NewEntryActivity extends AppCompatActivity {
 
-    Button dateButton;
-    Calendar mDate;
-    int day, month, year;
+    TextView dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+        setTitle("Dodaj wpis");
 
-        dateButton = findViewById(R.id.date_picker);
+        dateTextView = findViewById(R.id.date_picker);
 
-        mDate = Calendar.getInstance();
-
-        day = mDate.get(Calendar.DAY_OF_MONTH);
-        month = mDate.get(Calendar.MONTH);
-        year = mDate.get(Calendar.YEAR);
-
-        //month = month + 1;
-
-        dateButton.setOnClickListener(new View.OnClickListener() {
+        dateTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(NewEntryActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Intent datePickIntent = new Intent(NewEntryActivity.this, NewEntryDatePickActivity.class);
 
-                        //akcja po zatwierdzeniu wyboru daty
+                NewEntryActivity.this.startActivity(datePickIntent);
 
-                    }
-                }, year, month, day);
-                datePickerDialog.show();
             }
 
 
@@ -54,9 +37,10 @@ public class NewEntryActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_new_entry, menu);
         return true;
+
     }
 
     @Override
@@ -66,6 +50,7 @@ public class NewEntryActivity extends AppCompatActivity {
             case R.id.action_check:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                finish();
                 return true;
 
             default:
