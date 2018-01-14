4348180e7e180e39048a9d9a02023cdb5b4bb706
tdobrowolski <tobdob15@gmail.com>
2018-01-14 15:14:49 +0100

    Lekki redesign
    - zmiana wyglądu NewEntryActivity

--- app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
+++ app/src/main/java/com/example/tobiaszdobrowo/codenametreasure/NewEntryActivity.java
@@ -1,16 +1,23 @@
 package com.example.tobiaszdobrowo.codenametreasure;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class NewEntryActivity extends AppCompatActivity {
 
     TextView dateTextView;
+    TextView datePicker;
+    int year, month, day;
+    Calendar mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +25,30 @@ public class NewEntryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_entry);
         setTitle("Dodaj wpis");
 
-        dateTextView = findViewById(R.id.date_picker);
+        datePicker = findViewById(R.id.text_pick_date);
 
-        dateTextView.setOnClickListener(new View.OnClickListener() {
+        mDate = Calendar.getInstance();
+
+        day = mDate.get(Calendar.DAY_OF_MONTH);
+        month = mDate.get(Calendar.MONTH);
+        year = mDate.get(Calendar.YEAR);
+
+        datePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent datePickIntent = new Intent(NewEntryActivity.this, NewEntryDatePickActivity.class);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NewEntryActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-                NewEntryActivity.this.startActivity(datePickIntent);
+                        //akcja po zatwierdzeniu wyboru daty
+
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+
             }
-
-
         });
     }
 
